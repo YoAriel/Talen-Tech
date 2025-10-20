ef3e5c97f361154a6ff1042e1908c2149efd5730
Ariel Diaz <mtgkm1@gmail.com>
2025-10-20 18:23:35 -0300

    Corrige import de useCartContext para compatibilidad con Netlify

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,4 @@
-import { useCartContext } from "../../context/CartContext/UseCartContext";
+import { useCartContext } from "../../context/CartContext/useCartContext";
 import { Item } from "../Item/Item";
 import "./ItemDetail.css";
 
